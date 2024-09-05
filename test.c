411993dd7e6ece78701f89b3d215fed64492f932
mou7s <liuchuge7777777@gmail.com>
2024-09-05 16:35:36 +0800

    Update test.c

--- test.c
+++ test.c
@@ -1,26 +1,6 @@
-//fibonacci 递归
-
 #include <stdio.h>
 
-int fibonacci(int n)
-{
-    if (n == 0 || n == 1)
-    {
-        return n;
-    }
-    else
-    {
-        return fibonacci(n - 1) + fibonacci(n - 2);
-    }
-}
-
-int main()
-{
-    int n;
-    printf("请输入一个整数：");
-    scanf("%d", &n);
-    printf("斐波那契数列的第%d项为%d\n", n, fibonacci(n));
+int main() {
+    printf("Hello, World!\n");
     return 0;
 }
-
-
